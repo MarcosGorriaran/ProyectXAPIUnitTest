974473a7098396972343e06efa50e4736bc5123f
Marcos <marcos.gorriaran.7e7@itb.cat>
2025-02-14 17:46:39 +0100

    Atempting to make a request

---
+++ ProyectXAPILibrariUnitTest/AcountControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectXAPI.Models;
+using ProyectXAPILibrary.Controller;
+using System;
+using System.Net.Http;
+
+namespace ProyectXAPILibrariUnitTest
+{
+    [TestClass]
+    public class AcountControllerTest
+    {
+        const string APIAddress = "http://localhost:5000";
+        [TestMethod]
+        public void SuccesfulCheckLogin()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(APIAddress);
+            AcountController controller = new AcountController(client);
+            Acount acount = new Acount()
+            {
+                Username = "ME",
+                Password = "ME"
+            };
+
+            controller.CheckLoginRequest(acount);
+        }
+    }
+}
---
+++ ProyectXAPILibrariUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProyectXAPILibrariUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Departament d'Educació;")]
+[assembly: AssemblyProduct("ProyectXAPILibrariUnitTest")]
+[assembly: AssemblyCopyright("Copyright © Departament d'Educació; 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81c81206-3c0c-42d9-a4df-168a660ccf95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
